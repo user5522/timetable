fbe77f456e61455b45ceb5a89b97b6bd08ab88ea
Username * <oueslatim@pm.me>
2023-09-12 13:40:52 +0100

    figure out a way to save & load settings using shared_preferences

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,4 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings {
   final bool compactMode;
@@ -24,13 +25,16 @@ class Settings {
 }
 
 class SettingsNotifier extends StateNotifier<Settings> {
-  SettingsNotifier() : super(Settings());
+  SettingsNotifier() : super(Settings()) {
+    loadSettings();
+  }
 
   void updateCompactMode(bool compactMode) {
     final newState = state.copy(
       compactMode: compactMode,
     );
     state = newState;
+    _saveCompactMode(compactMode);
   }
 
   void updateHideLocation(bool hideLocation) {
@@ -38,6 +42,7 @@ class SettingsNotifier extends StateNotifier<Settings> {
       hideLocation: hideLocation,
     );
     state = newState;
+    _saveHideLocation(hideLocation);
   }
 
   void updateSingleLetterDays(bool singleLetterDays) {
@@ -45,6 +50,59 @@ class SettingsNotifier extends StateNotifier<Settings> {
       singleLetterDays: singleLetterDays,
     );
     state = newState;
+    _saveSingleLetterDays(singleLetterDays);
+  }
+
+  Future<void> loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    await _loadCompactMode(prefs);
+    await _loadSingleLetterDays(prefs);
+    await _loadHideLocation(prefs);
+  }
+
+  Future<void> _loadCompactMode(SharedPreferences prefs) async {
+    final compactMode = prefs.getBool('compactMode');
+    if (compactMode != null) {
+      state = state.copy(compactMode: compactMode);
+    }
+  }
+
+  Future<void> _saveCompactMode(bool compactModeValue) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(
+      'compactMode',
+      compactModeValue,
+    );
+  }
+
+  Future<void> _loadHideLocation(SharedPreferences prefs) async {
+    final hideLocation = prefs.getBool('hideLocation');
+    if (hideLocation != null) {
+      state = state.copy(hideLocation: hideLocation);
+    }
+  }
+
+  Future<void> _saveHideLocation(bool hideLocationValue) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(
+      'hideLocation',
+      hideLocationValue,
+    );
+  }
+
+  Future<void> _loadSingleLetterDays(SharedPreferences prefs) async {
+    final singleLetterDays = prefs.getBool('singleLetterDays');
+    if (singleLetterDays != null) {
+      state = state.copy(singleLetterDays: singleLetterDays);
+    }
+  }
+
+  Future<void> _saveSingleLetterDays(bool singleLetterDaysValue) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(
+      'singleLetterDays',
+      singleLetterDaysValue,
+    );
   }
 }
 
